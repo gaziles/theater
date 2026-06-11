e9127235827016e5aef1d0daaa73c72fbe550225
gaziles <eliza.gajda@student.wat.edu.pl>
2026-06-11 08:13:50 +0200

    feat: add coordinates for managment

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from theater_lib.controller import *
+from theater_lib.map import (
+    generate_theater_map,
+    generate_clients_map,
+    generate_employees_map
+)
 
 
 def menu():
@@ -13,6 +18,11 @@ def menu():
     print("8 - wyświetl pracowników")
     print("9 - dodaj pracownika")
     print("10 - usuń pracownika")
+    print("11 - wyświetl spektakle klienta")
+    print("12 - dodaj spektakl klientowi")
+    print("13 - mapa teatrów")
+    print("14 - mapa klientów")
+    print("15 - mapa pracowników")
 
 
 def main():
@@ -55,6 +65,21 @@ def main():
 
         elif choice == "10":
             delete_employee()
+
+        elif choice == "11":
+            show_client_performances()
+
+        elif choice == "12":
+            add_performance_to_client()
+
+        elif choice == "13":
+            generate_theater_map()
+
+        elif choice == "14":
+            generate_clients_map()
+
+        elif choice == "15":
+            generate_employees_map()
 
         else:
             print("Nieprawidłowa opcja")
--- theater_lib/model.py
+++ theater_lib/model.py
@@ -3,121 +3,146 @@ theaters = [
     {
         'name': 'Teatr Muzyczny Roma',
         'location': 'Warszawa',
-        'performances': ['Hamilton', 'Cats'],
-        'clients': ['Anna', 'Kamil'],
-        'employees': ['Jan Kowalski', 'Maria Nowak']
+
+        'clients': [
+            {
+                'name': 'Anna Nowak',
+                'location': 'Warszawa',
+                'performances': ['Hamilton', 'Cats', 'Chicago']
+            },
+            {
+                'name': 'Kamil Śruba',
+                'location': 'Płock',
+                'performances': ['Cats', 'Hamilton']
+            }
+        ],
+
+        'employees': [
+            {
+                'name': 'Jan Kowalski',
+                'location': 'Sochaczew'
+            },
+            {
+                'name': 'Maria Nowak',
+                'location': 'Radom'
+            }
+        ]
     },
 
     {
         'name': 'Teatr Narodowy',
-        'location': 'Warszawa',
-        'performances': ['Dziady', 'Wesele'],
-        'clients': ['Piotr', 'Magda'],
-        'employees': ['Adam Nowak', 'Karolina Lis']
+        'location': 'Kraków',
+
+        'clients': [
+            {
+                'name': 'Piotr Zieliński',
+                'location': 'Kraków',
+                'performances': ['Dziady', 'Wesele']
+            },
+            {
+                'name': 'Magda Lis',
+                'location': 'Wieliczka',
+                'performances': ['Wesele', 'Dziady', 'Hamlet']
+            }
+        ],
+
+        'employees': [
+            {
+                'name': 'Adam Nowak',
+                'location': 'Zakopane'
+            },
+            {
+                'name': 'Karolina Lis',
+                'location': 'Częstochowa'
+            }
+        ]
     },
 
     {
         'name': 'Teatr Komedia',
-        'location': 'Warszawa',
-        'performances': ['Mayday', 'Boeing Boeing'],
-        'clients': ['Tomasz', 'Ewa'],
-        'employees': ['Paweł Wójcik', 'Agnieszka Mazur']
-    },
-
-    {
-        'name': 'Teatr Bagatela',
-        'location': 'Kraków',
-        'performances': ['Hamlet', 'Makbet'],
-        'clients': ['Ola', 'Paweł'],
-        'employees': ['Piotr Zieliński', 'Ewa Lis']
-    },
-
-    {
-        'name': 'Teatr Ludowy',
-        'location': 'Kraków',
-        'performances': ['Zemsta', 'Balladyna'],
-        'clients': ['Monika', 'Kacper'],
-        'employees': ['Tadeusz Król', 'Joanna Kania']
-    },
-
-    {
-        'name': 'Teatr STU',
-        'location': 'Kraków',
-        'performances': ['Biesy', 'Król Lear'],
-        'clients': ['Natalia', 'Michał'],
-        'employees': ['Rafał Sikora', 'Anna Duda']
-    },
-
-    {
-        'name': 'Teatr Polski',
-        'location': 'Wrocław',
-        'performances': ['Makbet', 'Wesele'],
-        'clients': ['Karolina', 'Tomek'],
-        'employees': ['Adam Wiśniewski', 'Natalia Kaczmarek']
-    },
-
-    {
-        'name': 'Teatr Capitol',
-        'location': 'Wrocław',
-        'performances': ['Mistrz i Małgorzata', 'Hair'],
-        'clients': ['Patryk', 'Julia'],
-        'employees': ['Szymon Krawczyk', 'Ewa Baran']
-    },
-
-    {
-        'name': 'Teatr Lalek',
-        'location': 'Wrocław',
-        'performances': ['Pinokio', 'Kopciuszek'],
-        'clients': ['Kinga', 'Marcin'],
-        'employees': ['Jan Urban', 'Alicja Wrona']
-    },
-
-    {
-        'name': 'Teatr Wybrzeże',
         'location': 'Gdańsk',
-        'performances': ['Hamlet', 'Zbrodnia i kara'],
-        'clients': ['Robert', 'Zuzanna'],
-        'employees': ['Mateusz Pawlak', 'Monika Sowa']
+
+        'clients': [
+            {
+                'name': 'Tomasz Wiśniewski',
+                'location': 'Gdańsk',
+                'performances': ['Mayday', 'Boeing Boeing']
+            },
+            {
+                'name': 'Ewa Kaczmarek',
+                'location': 'Sopot',
+                'performances': ['Mayday', 'Kolacja dla głupca']
+            }
+        ],
+
+        'employees': [
+            {
+                'name': 'Paweł Wójcik',
+                'location': 'Koszalin'
+            },
+            {
+                'name': 'Agnieszka Mazur',
+                'location': 'Gdynia'
+            }
+        ]
     },
 
     {
-        'name': 'Teatr Miniatura',
-        'location': 'Gdańsk',
-        'performances': ['Alicja w Krainie Czarów', 'Piotruś Pan'],
-        'clients': ['Marta', 'Bartek'],
-        'employees': ['Andrzej Bąk', 'Katarzyna Lis']
+        'name': 'Teatr Horyzont',
+        'location': 'Wrocław',
+
+        'clients': [
+            {
+                'name': 'Natalia Dąbrowska',
+                'location': 'Wrocław',
+                'performances': ['Makbet', 'Hamlet', 'Otello']
+            },
+            {
+                'name': 'Michał Sikora',
+                'location': 'Legnica',
+                'performances': ['Makbet', 'Król Lear']
+            }
+        ],
+
+        'employees': [
+            {
+                'name': 'Rafał Kurek',
+                'location': 'Zielona Góra'
+            },
+            {
+                'name': 'Joanna Maj',
+                'location': 'Łódź'
+            }
+        ]
     },
 
     {
-        'name': 'Teatr Szekspirowski',
-        'location': 'Gdańsk',
-        'performances': ['Romeo i Julia', 'Otello'],
-        'clients': ['Damian', 'Sandra'],
-        'employees': ['Tomasz Kurek', 'Iwona Maj']
-    },
+        'name': 'Teatr Panorama',
+        'location': 'Poznań',
 
-    {
-        'name': 'Teatr Powszechny',
-        'location': 'Radom',
-        'performances': ['Szalone nożyczki', 'Koziołek Matołek'],
-        'clients': ['Radek', 'Ilona'],
-        'employees': ['Jan Motyka', 'Szymon Kędzierski']
-    },
+        'clients': [
+            {
+                'name': 'Łukasz Pawlak',
+                'location': 'Poznań',
+                'performances': ['Hair', 'Mistrz i Małgorzata']
+            },
+            {
+                'name': 'Weronika Sowa',
+                'location': 'Toruń',
+                'performances': ['Hair', 'Cats', 'Chicago']
+            }
+        ],
 
-    {
-        'name': 'Teatr Kameralny',
-        'location': 'Radom',
-        'performances': ['Skąpiec', 'Moralność Pani Dulskiej'],
-        'clients': ['Marek', 'Joanna'],
-        'employees': ['Rafał Czarnecki', 'Anna Borkowska']
-    },
-
-    {
-        'name': 'Teatr Miejski',
-        'location': 'Radom',
-        'performances': ['Antygona', 'Zemsta'],
-        'clients': ['Łukasz', 'Weronika'],
-        'employees': ['Kamil Górski', 'Marta Sikora']
+        'employees': [
+            {
+                'name': 'Kamil Górski',
+                'location': 'Kalisz'
+            },
+            {
+                'name': 'Marta Sikora',
+                'location': 'Bydgoszcz'
+            }
+        ]
     }
 
 ]
